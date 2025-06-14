3255148c98dcfd5d920a1f79d5f949f67cd52960
Server048 <64312413+Server048@users.noreply.github.com>
2025-06-14 16:59:11 +0800

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -26,7 +26,7 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
     <ChakraProvider theme={theme}>
       <QueryClientProvider client={client}>
         <Head>
-          <title>Demo Bot21</title>
+          <title>GROWTREE | BOT CONTROLLER CPS</title>
         </Head>
         {getLayout(<Component {...pageProps} />)}
       </QueryClientProvider>
