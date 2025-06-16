539a94b701a51e9979213c5d41fcd39d3e995486
Server048 <64312413+Server048@users.noreply.github.com>
2025-06-16 15:58:58 +0800

    Update controller.tsx

--- src/pages/user/controller.tsx
+++ src/pages/user/controller.tsx
@@ -17,13 +17,13 @@ import AppLayout from '@/components/layout/app';
 import { iconUrl } from '@/api/discord';
 import Link from 'next/link';
 
-const HomePage: NextPageWithLayout = () => {
+const ControllerPage: NextPageWithLayout = () => {
   
 
-  return <PanelKontrol />;
+  return <Controller />;
 };
 
-export function PanelKontrol() {
+export function Controller() {
   
 
 
@@ -32,5 +32,5 @@ export function PanelKontrol() {
   return <></>;
 }
 
-HomePage.getLayout = (c) => <AppLayout>{c}</AppLayout>;
-export default HomePage;
+ConstrollerPage.getLayout = (c) => <AppLayout>{c}</AppLayout>;
+export default ConstrollerPage;
