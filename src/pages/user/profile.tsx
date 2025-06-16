8d41eeb085aae186a5185049e11bacda3b9cdfb1
Server048 <64312413+Server048@users.noreply.github.com>
2025-06-16 09:57:18 +0800

    Update profile.tsx

--- src/pages/user/profile.tsx
+++ src/pages/user/profile.tsx
@@ -1,62 +1,126 @@
-import { Box, Button, Flex, Stack, Text, useColorModeValue } from '@chakra-ui/react';
+import { Flex, Grid, Spacer, Text, VStack } from '@chakra-ui/layout';
 import {
-  HomeIcon,
-  ChevronRightIcon,
-  DocumentIcon,
-  RepeatIcon,
-  RocketIcon,
-  DownloadIcon,
-  SettingsIcon,
-  ArrowRightIcon,
-} from '@chakra-ui/icons';
+  Avatar,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  FormControl,
+  FormLabel,
+  Image,
+  useColorMode,
+  Box,
+} from '@chakra-ui/react';
+import { avatarUrl, bannerUrl } from '@/api/discord';
+import { SelectField } from '@/components/forms/SelectField';
+import { SwitchField } from '@/components/forms/SwitchField';
+import { languages, names, useLang } from '@/config/translations/provider';
+import { profile } from '@/config/translations/profile';
+import { IoLogOut } from 'react-icons/io5';
+import { useSettingsStore } from '@/stores';
+import { NextPageWithLayout } from '@/pages/_app';
+import AppLayout from '@/components/layout/app';
+import { useLogoutMutation } from '@/utils/auth/hooks';
+import { useSelfUser } from '@/api/hooks';
 
-interface SidebarProps {}
+/**
+ * User info and general settings here
+ */
+const ProfilePage: NextPageWithLayout = () => {
+  const user = useSelfUser();
+  const logout = useLogoutMutation();
+  const t = profile.useTranslations();
 
-const Sidebar: React.FC<SidebarProps> = () => {
-  const bg = useColorModeValue('gray.800', 'gray.700');
-  const color = useColorModeValue('white', 'gray.200');
+  const { colorMode, setColorMode } = useColorMode();
+  const { lang, setLang } = useLang();
+  const [devMode, setDevMode] = useSettingsStore((s) => [s.devMode, s.setDevMode]);
 
   return (
-    <Box
-      w="200px"
-      bg={bg}
-      boxShadow="md"
-      rounded="md"
-      p={4}
-      display={{ base: 'none', md: 'block' }}
-    >
-      <Flex mb={4} align="center">
-        <Box mr={2} w="40px" h="40px" rounded="full" bg="gray.300" />
-        <Text fontWeight="bold" fontSize="lg" color={color}>
-          GrowTree - PS
-        </Text>
-        <Text fontSize="sm" color={color}>Kelola bot Discord Anda</Text>
+    <Grid templateColumns={{ base: '1fr', lg: 'minmax(0, 800px) auto' }} gap={{ base: 3, lg: 6 }}>
+      <Flex direction="column">
+        {user.banner != null ? (
+          <Image
+            alt="banner"
+            src={bannerUrl(user.id, user.banner)}
+            sx={{ aspectRatio: '1100 / 440' }}
+            objectFit="cover"
+            rounded="2xl"
+          />
+        ) : (
+          <Box bg="Brand" rounded="2xl" sx={{ aspectRatio: '1100 / 440' }} />
+        )}
+        <VStack mt="-50px" ml="40px" align="start">
+          <Avatar
+            src={avatarUrl(user)}
+            name={user.username}
+            w="100px"
+            h="100px"
+            ringColor="CardBackground"
+            ring="6px"
+          />
+          <Text fontWeight="600" fontSize="2xl">
+            {user.username}
+          </Text>
+        </VStack>
       </Flex>
-      <Stack spacing={4}>
-        <Button variant="solid" colorScheme="purple" leftIcon={<HomeIcon />} color={color}>
-          Dashboard
-        </Button>
-        <Button leftIcon={<ChevronRightIcon />} color={color}>Perintah Bot</Button>
-        <Button leftIcon={<DocumentIcon />} color={color}>Log Bot</Button>
-        <Button leftIcon={<RepeatIcon />} color={color}>Log Pengguna</Button>
-        <Button leftIcon={<RocketIcon />} color={color}>Uji Bot</Button>
-        <Button leftIcon={<DownloadIcon />} color={color}>Ekspor Bot</Button>
-        <Button leftIcon={<SettingsIcon />} color={color}>Pengaturan</Button>
-        <Button variant="outline" colorScheme="red" rightIcon={<ArrowRightIcon />} color={color}>
-          Keluar
-        </Button>
-      </Stack>
-    </Box>
+      <Card w="full" rounded="3xl" h="fit-content" variant="primary">
+        <CardHeader fontSize="2xl" fontWeight="600">
+          {t.settings}
+        </CardHeader>
+        <CardBody as={Flex} direction="column" gap={6} mt={3}>
+          <SwitchField
+            id="dark-mode"
+            label={t['dark mode']}
+            desc={t['dark mode description']}
+            isChecked={colorMode === 'dark'}
+            onChange={(e) => setColorMode(e.target.checked ? 'dark' : 'light')}
+          />
+          <SwitchField
+            id="developer-mode"
+            label={t['dev mode']}
+            desc={t['dev mode description']}
+            isChecked={devMode}
+            onChange={(e) => setDevMode(e.target.checked)}
+          />
+          <FormControl>
+            <Box mb={2}>
+              <FormLabel fontSize="md" fontWeight="medium" m={0}>
+                {t.language}
+              </FormLabel>
+              <Text color="TextSecondary">{t['language description']}</Text>
+            </Box>
+            <SelectField
+              value={{
+                label: names[lang],
+                value: lang,
+              }}
+              onChange={(e) => e != null && setLang(e.value)}
+              options={languages.map((lang) => ({
+                label: lang.name,
+                value: lang.key,
+              }))}
+            />
+          </FormControl>
+          <Spacer />
+          <Button
+            leftIcon={<IoLogOut />}
+            variant="danger"
+            isLoading={logout.isLoading}
+            onClick={() => logout.mutate()}
+          >
+            {t.logout}
+          </Button>
+        </CardBody>
+      </Card>
+      <Content />
+    </Grid>
   );
 };
 
-const Controller: React.FC = () => {
-  return (
-    <Flex>
-      <Sidebar />
-      <Box ml={4} p={4} /> {/* Ruang kosong untuk konten utama */}
-    </Flex>
-  );
-};
+function Content() {
+  return <></>;
+}
 
-export default Controller;
+ProfilePage.getLayout = (p) => <AppLayout>{p}</AppLayout>;
+
+export default ProfilePage;
