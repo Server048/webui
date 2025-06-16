9979e388e509bd8397439086cc97f9a7c9e4e79e
Server048 <64312413+Server048@users.noreply.github.com>
2025-06-16 17:17:49 +0800

    Update controller.tsx

--- src/pages/user/controller.tsx
+++ src/pages/user/controller.tsx
@@ -192,7 +192,31 @@ const HomePage: NextPageWithLayout = () => {
 };
 
 export function Controller() {
-  
+  const SidebarWithHeader = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
+      <SidebarContent onClose={() => onClose} display={{ base: 'none', md: 'block' }} />
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        returnFocusOnClose={false}
+        onOverlayClick={onClose}
+        size="full"
+      >
+        <DrawerContent>
+          <SidebarContent onClose={onClose} />
+        </DrawerContent>
+      </Drawer>
+      {/* mobilenav */}
+      <MobileNav onOpen={onOpen} />
+      <Box ml={{ base: 0, md: 60 }} p="4">
+        {/* Content */}
+      </Box>
+    </Box>
+  );
+};
 
 
   
