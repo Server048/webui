b60b7bd048e12fdf37c1b9098fe4c005ef873784
Server048 <64312413+Server048@users.noreply.github.com>
2025-06-16 15:17:13 +0800

    Update controller.tsx

--- src/pages/user/controller.tsx
+++ src/pages/user/controller.tsx
@@ -6,7 +6,17 @@ import { iconUrl } from '@/api/discord';
 import HomeView from '@/config/example/HomeView';
 import { config } from '@/config/common';
 import { useGuilds } from '@/api/hooks';
-import { HomeIcon, Skeleton, SettingsIcon, RocketIcon, DocumentIcon, LogoutIcon, SimpleGrid } from '@chakra-ui/icons';
+import {
+  Heading,
+  Button,
+  Card,
+  CardHeader,
+  Avatar,
+  Flex,
+  SimpleGrid,
+  Skeleton,
+  Text,
+} from '@chakra-ui/react';
 
 
 const PanelKontrol: NextPageWithLayout = () => {
@@ -16,7 +26,28 @@ const PanelKontrol: NextPageWithLayout = () => {
 };
 export function PanelKontrol() {
   const guilds = useGuilds();
-    
+      if (guilds.status === 'success')
+    return (
+      <SimpleGrid columns={{ base: 1, md: 2, xl: 3 }} gap={3}>
+        {guilds.data
+          ?.filter((guild) => config.guild.filter(guild))
+          .map((guild) => (
+            <Card key={guild.id} variant="primary" as={Link} href={`/guilds/${guild.id}`}>
+              <CardHeader as={Flex} flexDirection="row" gap={3}>
+                <Avatar src={iconUrl(guild)} name={guild.name} size="md" />
+                <Text>{guild.name}</Text>
+              </CardHeader>
+            </Card>
+          ))}
+      </SimpleGrid>
+    );
+
+  if (guilds.status === 'error')
+    return (
+      <Button w="fit-content" variant="danger" onClick={() => guilds.refetch()}>
+        Try Again
+      </Button>
+    );
   if (guilds.status === 'loading')
     return (
       <SimpleGrid columns={{ base: 1, md: 2, xl: 3 }} gap={3}>
