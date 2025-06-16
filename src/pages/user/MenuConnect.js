44bb8a0f1304daa52b3b69c8d2dd7bfa90b13675
Server048 <64312413+Server048@users.noreply.github.com>
2025-06-16 10:06:30 +0800

    Create MenuConnect.js

---
+++ src/pages/user/MenuConnect.js
@@ -0,0 +1,15 @@
+// MenuConnect.js
+const MenuConnect = () => {
+  return (
+    <aside style={{ width: '200px', backgroundColor: '#f0f0f0', height: '100vh', position: 'fixed' }}>
+      {/* Konten sidebar Anda di sini */}
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </aside>
+  );
+};
+
+export default MenuConnect;
